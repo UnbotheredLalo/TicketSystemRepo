089f6b78953972938de7821505781fdf2d744f94
Eduardo.Valadez <eduardo.valadez197@hotmail.com>
2024-10-14 22:36:33 -0600

    modificacion del formato de los comentarios

--- src/management/TicketManagement.java
+++ src/management/TicketManagement.java
@@ -12,30 +12,32 @@ public interface TicketManagement {
     String getUMA(Ticket ticket);
     String getFineDescription(Ticket ticket);
     void deleteFine(long fineID);
-    void setTicket(long fineID);
-    /*
+
+    /**
     * This assigns a Ticket by assigning an ID manually; whether you use this method
-    or the constructor given on 'Ticket' class, you'll be creating a Ticket object
-    with a unique ID. This method throws an exception if the entered ID is repeated.
+    * or the constructor given on 'Ticket' class, you'll be creating a Ticket object
+    * with a unique ID. This method throws an exception if the entered ID is repeated.
 
-    * @params fineID, the fine ID is manually entered instead of being randomly generated.
+    * @param fineID, the fine ID is manually entered instead of being randomly generated.
      */
+    void setTicket(long fineID);
 
-    void addFineToList(String offenderKey);
-    /*
+    /**
     * This method adds a newly created fine to the list once it was assigned to the
-    offender by linking it with his/her/its license plate/CURP/NRP.
+    * offender by linking it with his/her/its license plate/CURP/NRP.
 
-    * @params offenderKey, the key which the offender gets identified with,
-    such as CURP (natural person), NRP (Legal entity) or the license plates
-    of the offender's car
+    * @param offenderKey, the key which the offender gets identified with,
+    * such as CURP (natural person), NRP (Legal entity) or the license plates
+    * of the offender's car
     */
+    void addFineToList(String offenderKey);
 
+    /**
+    * Gets the amount of the fine with the discount applied based on the fine's
+    * date application. It varies depending on the type of fine
+     *
+    */
     double getDiscount(ZonedDateTime issueDate);
-    /*
-    Gets the amount of the fine with the discount applied based on the fine's
-    date application.
-    It varies depending on the type of fine
-    */
+
 
 }
